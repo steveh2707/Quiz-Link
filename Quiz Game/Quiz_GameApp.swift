c2d7223ccbb6b17414f103658a01bf3090e68316
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-09-13 14:37:20 +0100

    Initial Commit

---
+++ Quiz Game/Quiz_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_GameApp.swift
+//  Quiz Game
+//
+//  Created by Steve on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
